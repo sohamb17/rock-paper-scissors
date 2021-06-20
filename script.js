36b226f15b50a57173aae54411bb6b83f7f8cc09
sohamb17 <sohambelurgikar17@gmail.com>
2021-06-20 19:48:09 +0530

    Create playRound() function

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ function getRandomElement(array) {
 
 function computerPlay() {
     let options = ["Rock", "Paper", "Scissors"];
-    console.log(getRandomElement(options));
+    return getRandomElement(options);
 }
 
-computerPlay();
+function capitalize(string) {
+    return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "Rock") {
+        if(computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors";
+        } else if(computerSelection === "Paper") {
+            return "You Lose! Paper beats Rock";
+        }
+    } else if(playerSelection === "Paper") {
+        if(computerSelection === "Rock") {
+            return "You Win! Paper beats Rock";
+        } else if(computerSelection === "Scissors") {
+            return "You Lose! Scissors beat Paper";
+        }
+    } else if(playerSelection === "Scissors") {
+        if(computerSelection === "Paper") {
+            return "You Win! Scissors beat Paper";
+        } else if(computerSelection === "Rock") {
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+    return "Draw";
+}
