d3388cd353bf1b9d7350a946889233fbff768356
sohamb17 <sohambelurgikar17@gmail.com>
2021-06-22 01:35:00 +0530

    Add DOM methods

--- script.js
+++ script.js
@@ -11,35 +11,37 @@ function capitalize(string) {
     return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
 }
 
+const div = document.querySelector("div");
+
 function playRound(playerSelection, computerSelection) {
     if(playerSelection === "Rock") {
         if(computerSelection === "Scissors") {
-            console.log("You Win! Rock beats Scissors");
+            div.textContent = "You Win! Rock beats Scissors";
             return 1;
         } else if(computerSelection === "Paper") {
-            console.log("You Lose! Paper beats Rock");
+            div.textContent = "You Lose! Paper beats Rock";
         } else if(computerSelection === "Rock") {
-            console.log("Draw");
+            div.textContent = "Draw";
         }
         return 0;
     } else if(playerSelection === "Paper") {
         if(computerSelection === "Rock") {
-            console.log("You Win! Paper beats Rock");
+            div.textContent = "You Win! Paper beats Rock";
             return 1;
         } else if(computerSelection === "Scissors") {
-            console.log("You Lose! Scissors beat Paper");
+            div.textContent = "You Lose! Scissors beat Paper";
         } else if(computerSelection === "Paper") {
-            console.log("Draw");
+            div.textContent = "Draw" ;
         }
         return 0;
     } else if(playerSelection === "Scissors") {
         if(computerSelection === "Paper") {
-            console.log("You Win! Scissors beat Paper");
+            div.textContent = "You Win! Scissors beat Paper";
             return 1;
         } else if(computerSelection === "Rock") {
-            console.log("You Lose! Rock beats Scissors");
+            div.textContent = "You Lose! Rock beats Scissors";
         } else if(computerSelection === "Scissors") {
-            console.log("Draw");
+            div.textContent = "Draw";
         }
         return 0;
     }
